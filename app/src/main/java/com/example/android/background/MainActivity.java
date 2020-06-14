27ea484819db4b0a5e0790c8a9b0abf54d40b958
michaelzz40 <61964609+michaelzz40@users.noreply.github.com>
2020-06-15 01:36:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/background/MainActivity.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.background;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.background.sync.ReminderTasks;
+import com.example.android.background.sync.ReminderUtilities;
+import com.example.android.background.sync.WaterReminderIntentService;
+import com.example.android.background.utilities.PreferenceUtilities;
+
+public class MainActivity extends AppCompatActivity implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+
+    private TextView mData;
+
+    private Toast mToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /** Get the views **/
+
+        mData = (TextView) findViewById(R.id.dataText);
+
+        /** Set the original values in the UI **/
+
+        updateData();
+        // COMPLETED (23) Schedule the charging reminder
+        ReminderUtilities.scheduleChargingReminder(this);
+
+        /** Setup the shared preference listener **/
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    /**
+     * Updates the TextView to display the new water count from SharedPreferences
+     */
+
+    /**
+     * Updates the TextView to display the new charging reminder count from SharedPreferences
+     */
+
+
+    private void updateData() {
+        String fetchedData = PreferenceUtilities.getResponseData(this);
+        mData.setText(fetchedData);
+    }
+
+    /**
+     * Adds one to the water count and shows a toast
+     */
+//    public void incrementWater(View view) {
+//        if (mToast != null) mToast.cancel();
+//        mToast = Toast.makeText(this, R.string.water_chug_toast, Toast.LENGTH_SHORT);
+//        mToast.show();
+//
+//        Intent incrementWaterCountIntent = new Intent(this, WaterReminderIntentService.class);
+//        incrementWaterCountIntent.setAction(ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
+//        startService(incrementWaterCountIntent);
+//    }
+
+    // COMPLETED (24) Remove the button and testNotification code
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        /** Cleanup the shared preference listener **/
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    /**
+     * This is a listener that will update the UI when the water count or charging reminder counts
+     * change
+     */
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if  (PreferenceUtilities.FETCHED_DATA.equals(key)){
+            updateData();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.background.sync;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+
+import com.example.android.background.utilities.NotificationUtils;
+import com.example.android.background.utilities.PreferenceUtilities;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class ReminderTasks {
+
+    public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
+    public static final String ACTION_DISMISS_NOTIFICATION = "dismiss-notification";
+    static final String ACTION_CHARGING_REMINDER = "charging-reminder";
+
+    public static void executeTask(Context context, String action) {
+        if (ACTION_INCREMENT_WATER_COUNT.equals(action)) {
+           new FetchData(context).execute();
+           NotificationUtils.clearAllNotifications(context);
+        } else if (ACTION_DISMISS_NOTIFICATION.equals(action)) {
+            NotificationUtils.clearAllNotifications(context);
+        } else if (ACTION_CHARGING_REMINDER.equals(action)) {
+            issueChargingReminder(context);
+        }
+    }
+
+    static class FetchData extends AsyncTask<Void,String,String>{
+        private Context c;
+
+        public FetchData(Context c) {
+            this.c = c;
+        }
+        String data = "";
+        @Override
+        protected String doInBackground(Void... strings) {
+
+            try {
+                URL searchUrl = new URL("https://jsonplaceholder.typicode.com/posts/54");
+                HttpsURLConnection httpsURLConnection = (HttpsURLConnection) searchUrl.openConnection();
+                InputStream inputStream = httpsURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = "";
+                while(line != null){
+                    line = bufferedReader.readLine();
+                    data = data + line;
+                }
+            } catch(MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            PreferenceUtilities.changeData(this.c, s);
+        }
+    }
+
+
+
+    // COMPLETED (2) Create an additional task for issuing a charging reminder notification.
+    // This should be done in a similar way to how you have an action for incrementingWaterCount
+    // and dismissing notifications. This task should both create a notification AND
+    // increment the charging reminder count (hint: there is a method for this in PreferenceUtilities)
+    // When finished, you should be able to call executeTask with the correct parameters to execute
+    // this task. Don't forget to add the code to executeTask which actually calls your new task!
+
+    private static void issueChargingReminder(Context context) {
+        PreferenceUtilities.incrementChargingReminderCount(context);
+        NotificationUtils.remindUserBecauseCharging(context);
+    }
+}
